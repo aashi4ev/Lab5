09edc59509ae9e9f13744f91bb25d4f7156eb541
aashi4ev <aarushiw@andrew.cmu.edu>
2017-10-31 18:40:37 -0400

    counter addeed

--- library.sv
+++ library.sv
@@ -217,6 +217,67 @@ module offset_check
     (input logic val, low, delta,
     output logic is_between);
 
-    
+   assign  is_between = (val>=low) && (val <=(low + delta));
 
 endmodule: offset_check
+
+module range_check_test;
+
+      logic val, low, high, is_between;
+
+      initial begin
+          $monitor();
+
+      end
+
+endmodule: range_check_test
+
+
+
+module Counter
+#(parameter WIDTH=8)
+(input logic clock, reset,
+ input logic [WIDTH-1:0] maxValue,
+ output logic [WIDTH-1:0] value);
+ 
+always_ff @(posedge clock, posedge reset)
+  if (reset || value == maxValue)
+    value <= 0;
+  else
+    value <= value + 1;
+    
+endmodule: Counter
+
+module Counter_test;
+logic clock, reset;
+logic [7:0] maxValue;
+logic [7:0] value;
+
+Counter #(8) c(.*);
+
+initial begin
+  $monitor ($stime,, "maxValue = %d value = %d", maxValue, value);
+  
+  clock = 0;
+  maxValue =  5;
+  reset = 1;
+  reset <= 0;
+  forever #5 clock = ~clock;
+end
+
+initial begin
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  @ (posedge clock);
+  $finish;
+end
+
+endmodule: Counter_test
