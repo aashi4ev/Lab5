fb466511d88abf452570de87086c5597da55de89
aashi4ev <aarushiw@andrew.cmu.edu>
2017-10-31 17:51:46 -0400

    task1 init

---
+++ task1.sv
@@ -0,0 +1,12 @@
+module range_check
+    (input logic val, low, high, 
+    output logic is_between);
+
+
+endmodule: range_check
+
+module offset_check
+    (input logic val, low, delta,
+    output logic is_between);
+
+endmodule: offset_check
